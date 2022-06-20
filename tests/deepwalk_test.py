f5ba3d56f7b0e24ff91cc82f3d55c84f64a3e1b3
浅梦 <weichen.swc@alibaba-inc.com>
2022-06-21 00:19:28 +0800

    ci

--- tests/deepwalk_test.py
+++ tests/deepwalk_test.py
@@ -1,13 +1,16 @@
-
-
-from ge import DeepWalk
 import networkx as nx
 
+from ge import DeepWalk
 
-if __name__ == "__main__":
+
+def test_DeepWalk():
     G = nx.read_edgelist('./Wiki_edgelist.txt',
                          create_using=nx.DiGraph(), nodetype=None, data=[('weight', int)])
 
     model = DeepWalk(G, walk_length=3, num_walks=2, workers=1)
     model.train(window_size=3, iter=1)
     embeddings = model.get_embeddings()
+
+
+if __name__ == "__main__":
+    pass
