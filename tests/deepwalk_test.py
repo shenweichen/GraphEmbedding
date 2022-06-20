ae8a14d6542064b52402e9964657d1ee0a923afd
浅梦 <weichen.swc@alibaba-inc.com>
2022-06-21 00:25:01 +0800

    ci

--- tests/deepwalk_test.py
+++ tests/deepwalk_test.py
@@ -4,7 +4,7 @@ from ge import DeepWalk
 
 
 def test_DeepWalk():
-    G = nx.read_edgelist('./Wiki_edgelist.txt',
+    G = nx.read_edgelist('./tests/Wiki_edgelist.txt',
                          create_using=nx.DiGraph(), nodetype=None, data=[('weight', int)])
 
     model = DeepWalk(G, walk_length=3, num_walks=2, workers=1)
